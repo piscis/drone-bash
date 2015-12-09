def0a867e58f1a5757f125875ce571d2a78b8b6b
Alex <self@pirsig.net>
2015-12-10 00:05:22 +0100

    Update main.go

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 
 	for _, c := range vargs.Commands {
 		fabArgs := strings.Split(c, " ")
-		c := exec.Command("fab", fabArgs...)
+		c := exec.Command(fabArgs...)
 		c.Stdout = os.Stdout
 		c.Stderr = os.Stderr
 		err := c.Run()
